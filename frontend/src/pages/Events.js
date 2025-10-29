5e2809448747a9960261aff6b1a0c0c08404553a
blasius-okwach <blasiusicdl@gmail.com>
2025-10-29 22:11:14 +0300

    Output a list of dummy events and Adding another (nested) layout route

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,3 +1,19 @@
-export default function EventsPage(){
-  return <h1>EventsPage</h1>
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  { id: "e1", title: "Mashujaa Day" },
+  { id: "e2", title: "Jamhuri Day" },
+];
+
+export default function EventsPage() {
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => <li key={event.id}>
+          <Link to={event.id}>{event.title}</Link>
+        </li>)}
+      </ul>
+    </>
+  );
 }
